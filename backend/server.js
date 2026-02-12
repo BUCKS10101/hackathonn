8c60dfbe6e8ea7b97463ed8446b9512d66bce468
Narayan N <narayan.n2024@vitstudent.ac.in>
2026-02-13 03:34:50 +0530

    establishing connection

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,10 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    const PORT = 5001;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
