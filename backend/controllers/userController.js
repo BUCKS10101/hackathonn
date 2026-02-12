10687f952a2f09b7dba2524d9b76110c6a314c53
shellyagarwal2176 <shellyagarwal2176@gmail.com>
2026-02-13 02:39:45 +0530

    Implement user registration and login functions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+const User = require("../models/user");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).json({ message: "User not found" });
+  res.json(user);
+};
